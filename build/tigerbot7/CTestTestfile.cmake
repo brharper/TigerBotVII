106fdc22676567efaf6c5485c35157cde99b6821
Bryan Harper <brh8447@rit.edu>
2016-12-28 14:51:47 -0800

    Initial commit from Bryan's Ubuntu 16.04 VM

---
+++ build/tigerbot7/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bryan/TigerBotVII/src/tigerbot7
+# Build directory: /home/bryan/TigerBotVII/build/tigerbot7
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
